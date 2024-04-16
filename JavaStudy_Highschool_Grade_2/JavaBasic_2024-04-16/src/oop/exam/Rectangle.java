70f87d504c7df58c9bc3224ea13b759a7154db63
Mirage <86884419+ttap112@users.noreply.github.com>
2024-04-16 20:29:09 +0900

    Update Rectangle.java

--- JavaStudy_Highschool_Grade_2/JavaBasic_2024-04-16/src/oop/exam/Rectangle.java
+++ JavaStudy_Highschool_Grade_2/JavaBasic_2024-04-16/src/oop/exam/Rectangle.java
@@ -1,10 +1,26 @@
 package oop.exam;
 
 public class Rectangle {
-    public int low;
-    public int col;
+    public int width;
+    public int height;
 
-    public static void TotalBox (int a, int b) {
+    void calculateArea() {
+        int total = width*height;
+        System.out.println("넓이 : "+total);
+    }
 
+    void calculatePerimeter() {
+        int total = (width*2)+(height*2);
+        System.out.println("둘레 길이 : "+total);
+    }
+
+    void isSquare() {
+        boolean type = false;
+        if (width == height) {
+            type = true;
+            System.out.println("정사각형 여부 : "+type);
+        } else {
+            System.out.println("정사각형 여부 : "+type);
+        }
     }
 }
